45d5da3f4eaadb5c903715912b6e24ce387db7e0
SeanMurphy-hub <scmurphy11031992@gmail.com>
2022-04-26 22:09:28 -0500

    Fixed page errors and cleaned up stylesheet

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+
 from flask import Flask
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -8,19 +9,31 @@ def index():
     return render_template('index.html', pageTitle='VTM Site')
 
 
-
-
-
 @app.route('/about')
 def about():
     return render_template('about.html', pageTitle='About')
 
+def calculate_top_area(radius):
+    return 3.14*(radius**2)
 
+def calculate_side_area(radius,height):
+    return 2*(3.14*(height*radius))
 
-
-
-@app.route('/estimate')
+@app.route('/estimate', methods=['GET','POST'])
 def estimate():
+    if request.method == 'POST':
+        form = request.form
+        radius = float(form['radius'])
+        height = float(form['height'])
+        pi = 3.14
+        topArea = pi * r**2
+        sidesArea = 2*(pi*(radius*height))
+        totalArea = topArea+sidesArea
+        totalSqFt = totalArea / 144
+        materialCost = 25 * totalSqFt
+        laborCost = 15 * totalSqFt
+        totalCostEstimate = "${:,.2f}".format(round(materialCost + laborCost, 2))
+        return (render_template('estimate.html', estimate = totalCostEstimate))
     return render_template('estimate.html', pageTitle='Estimator')
 
 if __name__ == '__main__':
